8018df0fa50d075b00a2903e9835fd5edad5d697
Robert ter Horst <roho@carapaxit.local>
2019-02-21 10:55:42 +0100

    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDTmACNvXz85Xa5fDk05bbjvgS+HlGAWOYyqLLNd6I9hqeFca2nVL5o2oSGAbbF1PNdnJDc86E8s1kqwlUSaYPZGsn7EFBsh8pl4h2Irr6SOJBs5B9wWCMUNYwyF0RqrYvcHsB3QLpogjOvb4wUnI8g5bF6M41Eznh8GjZlykrgQ4UKcBoaGRa9/p0fePet/9xN/TMa1QBA/k9lzEEhB1IRsmZv6o9LVclBuyf7mRxiv/J5xMiWkJ72jbWBU2p66uOqUbI325SMvrp4dE1OCVoDDAlA2XWOA+nKpF2IZVz56XGNlVHrCWga+a+WOysmJmDfj1OyBFsUys8ilUBl78Oi6H346EDUElIGg1/UmXp4LVUl7Xexoc+oYi1C7HnRl/+2m5ggbtYZ2XfcszoOxyizDKttNvnN8NJKiei1moNnQgjXtcEmomYtSCxqcfRL6dOY3a9Np0Cpkdm4uh+PRWnrePoQbJBCb0do4axUHTwQ8JqLT+2kiKFiVVhvMxMNfLYA1ubfdVuED9JAbnDyu+muU5PDWnfQ5W1dFw83mpPCQ++bV44Zk2zDfzBhEjH7FCGoT0Tm2NK3d0P9bvUnDxf9c08vudGGHJLC5V8/+KiwzPFAr6KsBN1nkShlqiuJQD5B1A9+zuWkFw2U+QzRn2ZTnDuHrA/WnDC8RaLvyuLo3Q== robert.ter.horst@carapaxit.nl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+//App.js
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FeaturedPost from './FeaturedPost';
+import OtherPosts from './OtherPosts';
+import SpotifyContainer from './SpotifyContainer';
+import * as CosmicFunctions from './cosmicFunctions';
 
 class App extends Component {
+
+    constructor(props){
+    super(props);
+    this.state = {
+       dataReceived: false,      
+       posts: [],
+       authors: [],
+       featuredPostIndex: 0,
+       otherPosts: [],
+    }
+  }
+
+  async componentDidMount(){
+    try {
+      const {posts, authors} = await CosmicFunctions.getCosmicJsData();
+      this.setState({dataReceived: true, posts: posts, authors: authors, otherPosts: posts.slice(1)})
+    }
+    catch(err) {
+      console.error('Error: Problem retrieving Cosmic JS data');
+      console.error(err);
+      console.error(err.stack);
+      this.setState({dataReceived: false});
+    }
+  }
+
+    changeFeaturedPost(index) {
+    let copyOfPosts = this.state.posts.slice();
+    copyOfPosts.splice(index, 1);
+    this.setState({featuredPostIndex: index, otherPosts: copyOfPosts});
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header">          
+          Old School Shuffle
         </header>
+        <div className="featuredPost">
+          {this.state.dataReceived ? <FeaturedPost post={this.state.posts[this.state.featuredPostIndex]}/> : ''}
+        </div>
+        <div className="spotifyPlayer">
+          <SpotifyContainer/>
+        </div>
+        <div className="otherPosts">
+          <OtherPosts allPosts={this.state.posts} otherPosts={this.state.otherPosts} changeFeaturedPost={(index) => this.changeFeaturedPost(index)}/>
+        </div>
+        <div className="footer">
+          <p>Footer here</p>
+        </div>
       </div>
     );
   }
---
+++ src/ConnectSpotify.js
@@ -0,0 +1,20 @@
+//ConnectSpotify.js
+
+import React, { Component } from 'react';
+import './ConnectSpotify.css';
+import * as SpotifyFunctions from './spotifyFunctions.js'
+
+class ConnectSpotify extends Component {
+
+  render() {
+    return (
+      <div className="ConnectSpotify">
+        <a href={SpotifyFunctions.redirectUrlToSpotifyForLogin()}>
+            <button>Connect to Spotify</button>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default ConnectSpotify;
